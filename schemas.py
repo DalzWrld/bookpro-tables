4924c78e6dccfbba904e49bc75cfa27fad8afee9
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-24 10:22:28 +0300

    feat: add schemas

--- schemas.py
+++ schemas.py
@@ -4,11 +4,11 @@ from marshmallow import Schema, ValidationError, fields, validates_schema
 class UserSchema(Schema):
     id = fields.Int(dump_only=True)
     username = fields.Str(required=True)
-    email = fields.Email(required=True)
+    email = fields.Email(required=True, unique=True)
     created_at = fields.DateTime()
     updated_at = fields.DateTime()
 
-class ProfileSchema(Schema):
+class DoctorSchema(Schema):
     id = fields.Int(dump_only=True)
     user_id = fields.Int(required=True)
     first_name = fields.Str(required=True)
@@ -17,6 +17,27 @@ class ProfileSchema(Schema):
     dob = fields.Date()
     gender = fields.Str()
     bio = fields.Str()
-    phone = fields.Str()
+    phone = fields.Str(unique=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+
+class PatientSchema(Schema):
+    id = fields.Int(dump_only=True)
+    user_id = fields.Int(required=True)
+    first_name = fields.Str(required=True)
+    last_name = fields.Str(required=True)
+    dob = fields.Date()
+    gender = fields.Str()
+    address = fields.Str()
+    phone = fields.Str(unique=True)
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+
+class HospitalSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    address = fields.Str()
+    phone = fields.Str(unique=True)
+    email = fields.Email(required=True, unique=True)
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
