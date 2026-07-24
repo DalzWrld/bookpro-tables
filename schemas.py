947f8d03d5763fad354305d5e697078017fb0532
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-24 10:40:01 +0300

    feat: add schemas

--- schemas.py
+++ schemas.py
@@ -39,6 +39,9 @@ class PatientSchema(Schema):
     created_at = fields.DateTime(dump_only=True)
     updated_at = fields.DateTime(dump_only=True)
 
+    appointments = fields.Nested("AppointmentSchema", many=True, exclude=("patient",))
+    reviews = fields.Nested("ReviewSchema", many=True, exclude=("patient",))
+
 class HospitalSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
