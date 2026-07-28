ba8cf52e7789a2556ae9d6d1f39a7995f80ef5d2
Ian Ng'ang'a <waithakaian49@gmail.com>
2026-07-28 23:59:21 +0300

    feat: add schemas

--- schemas.py
+++ schemas.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import Schema, ValidationError, fields, validate, validates_schema
 
 
 class UserSchema(Schema):
@@ -182,3 +182,36 @@ class ReviewListSchema(Schema):
     updated_at = fields.DateTime(dump_only=True)
 
 reviews_list_schema = ReviewListSchema(many=True)
+
+
+class LoginSchema(Schema):
+    email_address = fields.Email(required=True)
+    password = fields.Str(load_only=True)
+
+login_schema = LoginSchema()
+
+
+class RegisterSchema(Schema):
+    id = fields.Int(dump_only=True)
+    first_name = fields.Str(required=True)
+    last_name = fields.Str(required=True)
+    email_address = fields.Email(required=True)
+    phone = fields.Str(required=True)
+    password = fields.Str(load_only=True, validate=validate.Length(min=5))
+    created_at = fields.DateTime(dump_only=True)
+    updated_at = fields.DateTime(dump_only=True)
+
+    @validates_schema
+    def validate_schema(self, data, **kwargs):
+        errors = {}
+        if all(key in data for key in ["first_name", "last_name", "phone"]):
+            if len(data["first_name"]) < 1:
+                errors["first_name"] = ["Firstname is required"]
+            if len(data["last_name"]) < 1:
+                errors["last_name"] = ["Lastname is required"]
+            if len(data["phone"]) != 10:
+                errors["phone"] = ["phone number must be 10 characters"]
+        if errors:
+            raise ValidationError(errors)
+
+register_schema = RegisterSchema()
